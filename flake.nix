eb8546c3ee21d189048c6fc4ea37651b00ac60e0
Tamme Schichler <tamme@schichler.dev>
2025-06-01 09:35:00 +0000

    Enable experimental Nix features for improved functionality

--- flake.nix
+++ flake.nix
@@ -14,6 +14,8 @@
         modules = [
           ./configuration.nix
           {
+            nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
             environment.systemPackages = with pkgs; [
               starship
             ];
