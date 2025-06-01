2deb7c59bde1567430c1d9cf25429539b6f0e1a8
Tamme Schichler <tamme@schichler.dev>
2025-06-01 10:28:18 +0000

    Fix font configuration by changing 'fonts.fonts' to 'fonts.packages'

--- configuration.nix
+++ configuration.nix
@@ -94,7 +94,7 @@
     RuntimeMaxUse=200M
   '';
 
-  fonts.fonts = with pkgs; [ dejavu_fonts noto-fonts noto-fonts-cjk-sans noto-fonts-emoji ];
+  fonts.packages = with pkgs; [ dejavu_fonts noto-fonts noto-fonts-cjk-sans noto-fonts-emoji ];
 
   system.stateVersion = "24.05";
 }
