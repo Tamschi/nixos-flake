161cc5428cc9d6cf27f443e6d8b6ad715077c15c
Tamme Schichler <tamme@schichler.dev>
2025-06-01 10:35:22 +0000

    Refactor NixOS configuration by merging configuration.nix into flake.nix and updating system packages and settings

--- flake.nix
+++ flake.nix
@@ -12,20 +12,111 @@
       "Teclast-X4" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./configuration.nix
-          {
-            nix.settings.experimental-features = [ "nix-command" "flakes" ];
+          ({ config, pkgs, ... }: {
+            imports = [
+              ./hardware-configuration.nix
+            ];
 
+            # Enable the Budgie desktop environment
+            services.xserver.enable = true;
+            services.xserver.displayManager.lightdm.enable = true;
+            services.xserver.desktopManager.budgie.enable = true;
+
+            # Set system packages
             environment.systemPackages = with pkgs; [
+              nano # Replacing vim with nano as the text editor
+              git
               starship
             ];
+
+            # Configure basic settings
+            time.timeZone = "UTC";
+            networking.hostName = "Teclast-X4";
+
+            # Set the default locale
+            i18n.defaultLocale = "en_GB.UTF-8";
+
+            # Set the keyboard layout for the console
+            console.keyMap = "us";
+
+            # Set the keyboard layout for the X server
+            services.xserver.xkb = {
+              layout = "us";
+              variant = "intl";
+            };
+
+            # Configure PipeWire as the sole sound server
+            services.pulseaudio.enable = false;
+            hardware.alsa.enable = false;
+            services.pipewire = {
+              enable = true;
+              alsa.enable = false;
+              pulse.enable = true;
+            };
+
+            # Enable power management
+            powerManagement.enable = true;
+
+            # Enable the firewall
+            networking.firewall.enable = true;
+            networking.firewall.logRefusedConnections = true;
+
+            # User configuration
+            users.users.ts = {
+              isNormalUser = true;
+              extraGroups = [ "wheel" ]; # Grants sudo privileges
+            };
+
+            users.users.qz = {
+              isNormalUser = true;
+              extraGroups = [ "wheel" ]; # Grants sudo privileges
+            };
+
+            # Use systemd-boot instead of GRUB
+            boot.loader.systemd-boot.enable = true;
+            boot.loader.efi.canTouchEfiVariables = true;
+
+            # Enable hardware acceleration for video playback (Intel)
+            hardware.graphics.enable = true;
+            hardware.graphics.enable32Bit = true;
+            hardware.graphics.extraPackages = with pkgs; [
+              vaapiIntel
+              vaapiVdpau
+              libvdpau-va-gl
+              intel-media-driver
+            ];
+
+            # Enable automatic garbage collection
+            nix.gc.automatic = true;
+            nix.gc.dates = "weekly";
+            nix.gc.options = "--delete-older-than 7d";
+
+            # Set environment variables
+            environment.variables.EDITOR = "nano";
+            environment.variables.PAGER = "less";
+
+            # Enable systemd-timesyncd for time synchronization
+            services.timesyncd.enable = true;
+
+            # Enable zram swap
+            zramSwap.enable = true;
+
+            # Journal configuration
+            services.journald.extraConfig = ''
+              SystemMaxUse=500M
+              RuntimeMaxUse=200M
+            '';
+
+            fonts.packages = with pkgs; [ dejavu_fonts noto-fonts noto-fonts-cjk-sans noto-fonts-emoji ];
 
             programs.starship = {
               enable = true;
             };
 
             users.defaultUserShell = pkgs.nushell;
-          }
+
+            system.stateVersion = "24.05";
+          })
         ];
       };
     };
