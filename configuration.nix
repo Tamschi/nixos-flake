24abeda1d35ee67af16986e85a97f3971c309366
Tamme Schichler <tamme@schichler.dev>
2025-06-01 09:53:32 +0200

    I have no idea if this'll work 🤪

---
+++ configuration.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # Enable the Budgie desktop environment
+  services.xserver.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.budgie.enable = true;
+
+  # Set system packages
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+  ];
+
+  # Configure basic settings
+  time.timeZone = "UTC";
+  networking.hostName = "hostname";
+
+  # Enable sound
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+}
