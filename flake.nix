1f957d00a9c398da0f532870bb52f3eed110020f
Tamme Schichler <tamme@schichler.dev>
2025-06-01 10:13:39 +0200

    "Use nushell with starship prompt as default shell."

--- flake.nix
+++ flake.nix
@@ -3,14 +3,30 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nushell = {
+      url = "github:nushell/nushell";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, nushell }: {
     nixosConfigurations = {
       hostname = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+          {
+            environment.systemPackages = with pkgs; [
+              nushell
+              starship
+            ];
+
+            programs.starship = {
+              enable = true;
+              promptOrder = [ "username", "hostname", "directory", "git_branch" ];
+            };
+
+            users.defaultUserShell = pkgs.nushell;
+          }
         ];
       };
     };
